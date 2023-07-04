3bee3e4d16741c125e8787967a4a1ff55f2eacd9
emilioblacksmith <emilioblacksmithlush@gmail.com>
2023-07-04 17:09:37 -0600

    feat(platforms): create double/triple platform and start of crocodile script

---
+++ Assets/Scripts/Water Spawner/MovingCrocodile.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCrocodile : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector3.forward * (speed * Time.deltaTime));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // make damage and restart frog
+        }
+    }
+}
